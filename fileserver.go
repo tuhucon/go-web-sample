97d7e4321d7f10898e3f1609736ce1f6044ca7da
Tuan Nguyen <tuhucon@MacBook-Pro-cua-Tuan.local>
2024-08-09 09:44:38 +0700

    router, middleware, render, http2. Howerver, http2 still isn't enable curl -v --http2 "http://localhost:8080/persons/45" *   Trying [::1]:8080... * Connected to localhost (::1) port 8080 > GET /persons/45 HTTP/1.1 > Host: localhost:8080 > User-Agent: curl/8.4.0 > Accept: */* > Connection: Upgrade, HTTP2-Settings > Upgrade: h2c > HTTP2-Settings: AAMAAABkAAQAoAAAAAIAAAAA > < HTTP/1.1 200 OK < Content-Type: application/json; charset=UTF-8 < Date: Thu, 08 Aug 2024 14:43:12 GMT < Content-Length: 29 < * Connection #0 to host localhost left intact {"name":"tu hu con","age":40}% As you see, curl request http2 but server response http 1.1

---
+++ fileserver.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/unrolled/render"
+	"github.com/urfave/negroni/v3"
+	"golang.org/x/net/http2"
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+var renderer = render.New(render.Options{})
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", HomeHandler)
+	router.POST("/persons", PersonsPostHandler)
+	router.GET("/persons/:id", GetPersonsHandler)
+
+	middleware := negroni.New()
+	middleware.UseFunc(func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		start := time.Now()
+		next(rw, r)
+		slog.Info("request %s need %v", r.URL.Path, time.Since(start))
+	})
+	middleware.UseHandler(router)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: middleware,
+	}
+	err := http2.ConfigureServer(server, &http2.Server{
+		MaxHandlers:                  0,
+		MaxConcurrentStreams:         0,
+		MaxDecoderHeaderTableSize:    0,
+		MaxEncoderHeaderTableSize:    0,
+		MaxReadFrameSize:             0,
+		PermitProhibitedCipherSuites: false,
+		IdleTimeout:                  0,
+		MaxUploadBufferPerConnection: 0,
+		MaxUploadBufferPerStream:     0,
+		NewWriteScheduler:            nil,
+		CountError:                   nil,
+	})
+	if err != nil {
+		slog.Error("Failed to configure http2 server: %v", err)
+	}
+	err = server.ListenAndServe()
+	if err != nil {
+		slog.Error("server is stopped: %#v", err)
+	}
+}
+
+type Person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	_, err := fmt.Fprintln(w, "Welcome to the home page")
+	if err != nil {
+		slog.Error("Error writing to response: %#v", err)
+	}
+}
+
+func PersonsPostHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	_, err := fmt.Fprintln(w, "Post page")
+	if err != nil {
+		slog.Error("Error in PersonsPostHandler: %#v", err)
+	}
+}
+
+func GetPersonsHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	person := &Person{
+		Name: "tu hu con",
+		Age:  40,
+	}
+	err := renderer.JSON(w, http.StatusOK, person)
+	if err != nil {
+		slog.Error("Error in PersonsPostHandler: %#v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module fileserver
+
+go 1.22
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/unrolled/render v1.6.1
+	github.com/urfave/negroni/v3 v3.1.1
+	golang.org/x/net v0.28.0
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	golang.org/x/sys v0.23.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+)
