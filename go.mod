365e110a89e5fddeb45a2b199223c019b7f714b1
tuhucon <minhtuan93@yahoo.com>
2024-08-11 21:28:22 +0700

    refactor code test to use table testing

--- go.mod
+++ go.mod
@@ -15,10 +15,10 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
-	golang.org/x/sys v0.23.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/controller/person_controller_test.go
+++ internal/controller/person_controller_test.go
@@ -20,55 +20,41 @@ func (p *PersonRepositoryMock) FindAll() ([]*model.Person, error) {
 	return args.Get(0).([]*model.Person), args.Error(1)
 }
 
-func TestPersonController_GetAll_ReturnEmpty(t *testing.T) {
-	personRepositoryMock := new(PersonRepositoryMock)
-	personRepositoryMock.On("FindAll").Return([]*model.Person{}, nil)
-	personController := NewPersonController(render.New(), personRepositoryMock)
+func TestPersonController_GetAll(t *testing.T) {
+	table := []struct {
+		name string
+		data []*model.Person
+	}{
+		{"no data", []*model.Person{}},
+		{"one person", []*model.Person{{1, "tu hu con", 20}}},
+		{"two persons", []*model.Person{{1, "tu hu con", 20}, {2, "chich choe", 30}}},
+	}
+	for _, tt := range table {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			personRepositoryMock := new(PersonRepositoryMock)
+			personRepositoryMock.On("FindAll").Return(tt.data, nil)
+			personController := NewPersonController(render.New(), personRepositoryMock)
 
-	r := httptest.NewRequest(http.MethodGet, "/persons", nil)
-	w := httptest.NewRecorder()
-	err := personController.GetAll(w, r)
+			r := httptest.NewRequest(http.MethodGet, "/persons", nil)
+			w := httptest.NewRecorder()
+			err := personController.GetAll(w, r)
 
-	personRepositoryMock.AssertExpectations(t)
-	if err != nil {
-		t.Error(err)
-	}
-	if diff := cmp.Diff(w.Code, http.StatusOK); diff != "" {
-		t.Error(diff)
-	}
-	result := []*model.Person{}
-	err = json.Unmarshal(w.Body.Bytes(), &result)
-	if err != nil {
-		t.Error(err)
-	}
-	if diff := cmp.Diff(0, len(result)); diff != "" {
-		t.Error(diff)
-	}
-}
-
-func TestPersonController_GetAll_ReturnOne(t *testing.T) {
-	dataTest := []*model.Person{{1, "tu hu con", 12}}
-	personRepositoryMock := new(PersonRepositoryMock)
-	personRepositoryMock.On("FindAll").Return(dataTest, nil)
-	personController := NewPersonController(render.New(), personRepositoryMock)
-
-	r := httptest.NewRequest(http.MethodGet, "/persons", nil)
-	w := httptest.NewRecorder()
-	err := personController.GetAll(w, r)
-
-	personRepositoryMock.AssertExpectations(t)
-	if err != nil {
-		t.Error(err)
-	}
-	if diff := cmp.Diff(w.Code, http.StatusOK); diff != "" {
-		t.Error(diff)
-	}
-	var result []*model.Person
-	err = json.Unmarshal(w.Body.Bytes(), &result)
-	if err != nil {
-		t.Error(err)
-	}
-	if diff := cmp.Diff(dataTest, result); diff != "" {
-		t.Error(diff)
+			personRepositoryMock.AssertExpectations(t)
+			if err != nil {
+				t.Error(err)
+			}
+			if diff := cmp.Diff(w.Code, http.StatusOK); diff != "" {
+				t.Error(diff)
+			}
+			var result []*model.Person
+			err = json.Unmarshal(w.Body.Bytes(), &result)
+			if err != nil {
+				t.Error(err)
+			}
+			if diff := cmp.Diff(tt.data, result); diff != "" {
+				t.Error(diff)
+			}
+		})
 	}
 }
